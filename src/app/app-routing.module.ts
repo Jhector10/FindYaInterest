3b4a9571479599f850c7b94d3c57dd0bdb2fce04
Jhector10 <jashsmash10@gmail.com>
2021-04-11 02:21:13 -0400

    added routing between login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ForgotLoginPageComponent } from './forgot-login-page/forgot-login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: CreateAccountComponent},
+  { path: 'forgot', component: ForgotLoginPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
