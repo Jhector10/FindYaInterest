4a7a0fe598d852b0433498ddba89c425b7210ec1
sboamah <60356081+sboamah@users.noreply.github.com>
2021-04-12 20:11:16 -0400

    Form base for Account sidebar

---
+++ src/app/account-sidebar/account-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-sidebar',
+  templateUrl: './account-sidebar.component.html',
+  styleUrls: ['./account-sidebar.component.css']
+})
+export class AccountSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
