7aa678f39aa76d58ae2163a7975f0e6692a49873
Gavin <gsnyder@stevens.edu>
2021-04-20 23:38:56 -0400

    added images

---
+++ src/app/message-board/HSS200/HSS200.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-HSS200',
+  templateUrl: './HSS200.component.html',
+  styleUrls: ['../message-board.component.css']
+})
+export class HSS200Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
